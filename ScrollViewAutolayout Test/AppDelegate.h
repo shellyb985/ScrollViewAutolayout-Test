f06d03963044fa78d470130d71a8026a45c62fe4
Shelly Pritchard <shelly.pritchard@merucabs.com>
2017-06-19 15:09:30 +0530

    init

---
+++ ScrollViewAutolayout Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewAutolayout Test
+//
+//  Created by Shelly Pritchard on 17/06/17.
+//  Copyright © 2017 SPB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollViewAutolayout Test/ViewController.m
@@ -0,0 +1,42 @@
+//
+//  ViewController.m
+//  ScrollViewAutolayout Test
+//
+//  Created by Shelly Pritchard on 17/06/17.
+//  Copyright © 2017 SPB. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+ 
+ NOTES :
+ 
+ FIRST SET AUTOLAYOUT TO SCROLL VIEW (0,0,0,0)
+ ADD CONTENT VIEW 
+ SET AUTOLAYOUT (0,0,0,0)
+ CONTENT VIEW T0 SCROLLVIEW SUPERVIEW EQUAL WIDTH
+ ADD CONTENT AND SET HEIGHT ACCORDING TO CONTENT `
+ 
+ 
+ */
+
+
+@end
